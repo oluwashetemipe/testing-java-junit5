81e4f9a08b28337555a2d1b586e3dfe3cbdf4a61
Oluwashetemipe <oluwashetemipe@gmail.com>
2022-10-21 11:18:32 +0100

    display-name

--- src/test/java/guru/springframework/sfgpetclinic/controllers/IndexControllerTest.java
+++ src/test/java/guru/springframework/sfgpetclinic/controllers/IndexControllerTest.java
@@ -1,6 +1,7 @@
 package guru.springframework.sfgpetclinic.controllers;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -11,12 +12,14 @@ class IndexControllerTest {
     void setUp() {
         controller = new IndexController();
     }
+    @DisplayName("Test Proper view")
     @Test
     void index(){
         assertEquals("index",controller.index());
         assertEquals("index",controller.index(),"Wrong view returned");
         assertEquals("index",controller.index(),"Another wrong view returned");
     }
+    @DisplayName("Test Exception")
     @Test
     void oupsHandler(){
         assertTrue("notimplemented".equals(controller.oupsHandler()), () ->"This is some expensive " +
