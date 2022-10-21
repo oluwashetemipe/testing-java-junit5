0765639be5aa21e0513426eff48182c304268350
Oluwashetemipe <oluwashetemipe@gmail.com>
2022-10-21 12:02:51 +0100

    testing--timeouts

--- src/test/java/guru/springframework/sfgpetclinic/controllers/IndexControllerTest.java
+++ src/test/java/guru/springframework/sfgpetclinic/controllers/IndexControllerTest.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.time.Duration;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class IndexControllerTest {
@@ -23,5 +25,19 @@ class IndexControllerTest {
     @Test
     void oupsHandler(){
         assertThrows(ValueNotFoundException.class,() ->{controller.oupsHandler();});
+    }
+    @Test
+    void testTimeOut(){
+        assertTimeout(Duration.ofMillis(100), ()->{
+            Thread.sleep(5000);
+        });
+        System.out.println("I got here");
+    }
+    @Test
+    void testTimeOutPremptivly(){
+        assertTimeoutPreemptively(Duration.ofMillis(100), ()->{
+            Thread.sleep(5000);
+        });
+        System.out.println("I got here 3442");
     }
 }
